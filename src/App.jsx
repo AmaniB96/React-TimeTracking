511e60a39fda44fd71b0fb3da4d9041fb5840164
amani <bataganiramani@gmail.com>
2025-05-13 14:15:15 +0200

    weekly toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import './App.css'
 import Card from './assets/component/card'
 import data from '../public/data/data.json'
-
+import { useState } from 'react'
 
 
 function App() {
+  const[timeframe,setTimeframe] = useState('daily')
+
+  const toggleWeekly = () => {
+    setTimeframe('weekly')
+  }
 
   return (
-    <div className="card-container">
-        {data.map((item, index) => (
-            <Card key={index} title={item.title}/>
-        ))}
+    <>
+    <div className='section1'>
+      <div className='bigcard'>
+        <button id="toggle" onClick={toggleWeekly}>Weekly</button>
+      </div>
 
-    </div>
+      <div className="card-container">
+          {data.map((item, index) => (
+              <Card key={index} donnee={item} timeframe={timeframe} />
+          ))}
+      </div>
+      
+      </div>
+    </>
   )
 }
 
--- src/assets/component/card.jsx
+++ src/assets/component/card.jsx
@@ -1,18 +1,44 @@
 import './card.css'
 
 
-export default function Card({title}) {
-    
+export default function Card({donnee, timeframe}) {
+// background: url("../images/icon-work.svg");
+
+    const backgroundColors = {
+        'Work': 'hsl(15, 100%, 70%)',      
+        'Play': 'hsl(195, 74%, 62%)',      
+        'Study': 'hsl(348, 100%, 68%)',    
+        'Exercise': 'hsl(145, 58%, 55%)',  
+        'Social': 'hsl(264, 64%, 52%)',   
+        'Self Care': 'hsl(43, 84%, 65%)'   
+    }
+
+    const backgroundImages = {
+        'Work': 'url("../images/icon-work.svg")',      
+        'Play': 'url("../images/icon-play.svg")',      
+        'Study': 'url("../images/icon-study.svg)',    
+        'Exercise': 'url("../images/icon-exercise.svg)',  
+        'Social': 'url("../images/icon-social.svg)',   
+        'Self Care': 'url("../images/icon-self-care.svg)'   
+    }
+
+    const cardStyle = {
+        backgroundColor: backgroundColors[donnee.title],
+        backgroundImage: backgroundImages[donnee.title],
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: '90% -10%',
+        backgroundSize: '30%'
+    }
 
     return (
         <>
 
-        <div id="card">
-            <div id="sousCard">
-                <p>{title}</p> 
+        <div id="card" style= {cardStyle}>
+            <div className="sousCard">
+                <p>{donnee.title}</p>
+                <h2>{donnee.timeframes[timeframe].current} hrs</h2>
+                <p>Last week - {donnee.timeframes[timeframe].previous} hrs</p>
             </div>
-            
-
         </div>
         </>
     )
