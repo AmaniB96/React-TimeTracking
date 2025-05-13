235cd6c4a5a35b055c114e26be66526f2de09cc5
amani <bataganiramani@gmail.com>
2025-05-13 09:46:54 +0200

    first commit

---
+++ src/assets/component/card.jsx
@@ -0,0 +1,19 @@
+import './card.css'
+
+export default function Card() {
+    
+
+    return (
+        <>
+        <div id="sousCard">
+            <img src="" alt="" />
+        </div>
+
+        <div id="card">
+            <div className=''></div>
+            <div className=''></div>
+            <div className=''></div>
+        </div>
+        </>
+    )
+}
